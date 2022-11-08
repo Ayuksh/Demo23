62a21b2ff3dab334a3b154edf6a5cde39aacde52
admin <ayuksh05@gmail.com>
2022-11-08 09:08:01 +0530

    3rd commit

--- src/package1/Program2.java
+++ src/package1/Program2.java
@@ -3,5 +3,10 @@ package package1;
 public class Program2 {
     public static void main(String[] args) {
         System.out.println("PROGRAM 2 !!");
+        display();
+    }
+
+    public static void display(){
+        System.out.println("DISPLAY METHOD !!");
     }
 }
